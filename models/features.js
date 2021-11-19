f48d5239a1126032cb6cd367e5b9fcb7bc021dab
vinhle000 <vinhle000@gmail.com>
2021-11-18 22:01:32 -0800

    Implemeted getFeatures function for Feature model -retreives list of feature information form DB

--- models/features.js
+++ models/features.js
@@ -1,6 +1,8 @@
-const { Sequelize, DataTypes } = require('sequelize');
+const { Sequelize, DataTypes, Op } = require('sequelize');
 const {sequelize} = require('../db/index.js')
-
+const logger = require('pino')({
+  level: 'debug'
+})
 
 //id,product_id,feature,value
 const Feature = sequelize.define('feature', {
@@ -20,6 +22,34 @@ const Feature = sequelize.define('feature', {
   },
 }, { timestamps: false})
 
+const getFeatures = async function(productID){
+
+  let featureRecords = await Feature.findAll({
+    where: {
+      product_id: {
+        [Op.eq]: 2
+     }
+    }
+  });
+  if (featureRecords === null) {
+    // console.log(featureRecords)
+    logger.error(featureRecords)
+  } else {
+    // console.log(featureRecords)
+    let featureList = []
+    // logger.info(featureRecords);
+    for (let featureItem of featureRecords) {
+      let featureInfoItem = {
+        feature: featureItem.dataValues.feature,
+        value: featureItem.dataValues.value
+      }
+      featureList.push(featureInfoItem)
+    }
+    logger.info(featureList)
+    return featureList;
+  }
+
+}
 
 module.exports.Feature = Feature;
-
+module.exports.getFeatures = getFeatures;
